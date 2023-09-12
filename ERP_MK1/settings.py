b8e21ab783d339c8bfc68139524ef53658f72ed1
sanjaybaswa_pa <sanjaybaswa@titan.co.in>
2023-09-12 13:42:11 +0000

    commiting from pyton anywhere

--- ERP_MK1/settings.py
+++ ERP_MK1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!f*nj&-om1x3=@&=ko0*r^f3yva2jq)4kvhzq2xgnw(@lj(&@8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
